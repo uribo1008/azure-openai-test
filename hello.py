3130eea33c8cb23202fa08208ab1d33ec6c1d6f1
hjmr <2620990+hjmr@users.noreply.github.com>
2024-01-24 11:25:01 +0900

    slightly modified around environment variables handling.

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 import os
 from openai import AzureOpenAI
 
-client = AzureOpenAI(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), api_key=os.getenv("AZURE_OPENAI_KEY"), api_version="2023-05-15"
-)
+azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+azure_openai_key = os.getenv("AZURE_OPENAI_KEY")
+
+if azure_openai_endpoint is None or azure_openai_key is None:
+    raise Exception(
+        "Please set the environment variables AZURE_OPENAI_ENDPOINT and AZURE_OPENAI_KEY to your Azure OpenAI endpoint and API key."
+    )
+
+client = AzureOpenAI(azure_endpoint=azure_openai_endpoint, api_key=azure_openai_key, api_version="2023-05-15")
 
 response = client.chat.completions.create(
     model="hjmr_gpt35",
@@ -15,5 +21,4 @@ response = client.chat.completions.create(
     ],
 )
 
-for idx in range(len(response.choices)):
-    print(idx, response.choices[idx].message.content)
+print(response.choices[0].message.content)
